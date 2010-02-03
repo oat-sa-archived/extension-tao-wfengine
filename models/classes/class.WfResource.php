77b5af3c56288ef79524170b7bd61e4e2b7f83b6
llecaque <llecaque@793250d4-a296-4af0-ab9f-c080ea916244>
2010-02-03 15:31:58 +0000

    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@643 793250d4-a296-4af0-ab9f-c080ea916244

---
+++ models/classes/class.WfResource.php
@@ -0,0 +1,132 @@
+<?php
+
+error_reporting(E_ALL);
+
+/**
+ * WorkFlowEngine - class.WfResource.php
+ *
+ * $Id$
+ *
+ * This file is part of WorkFlowEngine.
+ *
+ * Automatic generated with ArgoUML 0.24 on 11.08.2008, 09:28:21
+ *
+ * @author firstname and lastname of author, <author@example.org>
+ */
+
+if (0 > version_compare(PHP_VERSION, '5')) {
+    die('This file was generated for PHP 5');
+}
+
+/* user defined includes */
+// section 10-13-1--31-740bb989:119ebfa9b28:-8000:0000000000000831-includes begin
+// section 10-13-1--31-740bb989:119ebfa9b28:-8000:0000000000000831-includes end
+
+/* user defined constants */
+// section 10-13-1--31-740bb989:119ebfa9b28:-8000:0000000000000831-constants begin
+// section 10-13-1--31-740bb989:119ebfa9b28:-8000:0000000000000831-constants end
+
+/**
+ * Short description of class WfResource
+ *
+ * @access public
+ * @author firstname and lastname of author, <author@example.org>
+ */
+class WfResource
+{
+    // --- ATTRIBUTES ---
+
+    /**
+     * Short description of attribute uri
+     *
+     * @access public
+     * @var string
+     */
+	
+	public $resource;
+    public $uri = '';
+
+    /**
+     * Short description of attribute label
+     *
+     * @access public
+     * @var string
+     */
+    public $label = '';
+
+    /**
+     * Short description of attribute comment
+     *
+     * @access public
+     * @var string
+     */
+    public $comment = '';
+
+    // --- OPERATIONS ---
+
+    /**
+     * Short description of method __construct
+     *
+     * @access public
+     * @author firstname and lastname of author, <author@example.org>
+     * @param string
+     * @return void
+     */
+    public function __construct($uri)
+    {
+        // section 10-13-1--31-740bb989:119ebfa9b28:-8000:00000000000008B2 begin
+		$this->uri =$uri;
+		$this->resource = new core_kernel_classes_Resource($uri,__METHOD__);
+		$this->setLabel($this->resource->getLabel());
+		$this->setComment($this->resource->comment);
+			
+        // section 10-13-1--31-740bb989:119ebfa9b28:-8000:00000000000008B2 end
+    }
+
+    /**
+     * Short description of method setLabel
+     *
+     * @access public
+     * @author firstname and lastname of author, <author@example.org>
+     * @param string
+     * @return void
+     */
+    public function setLabel($label)
+    {
+        // section 10-13-1--31-740bb989:119ebfa9b28:-8000:00000000000008DF begin
+		$this->label = str_replace(" ","&nbsp;",trim(strip_tags($label)));
+        // section 10-13-1--31-740bb989:119ebfa9b28:-8000:00000000000008DF end
+    }
+
+    /**
+     * Short description of method setComment
+     *
+     * @access public
+     * @author firstname and lastname of author, <author@example.org>
+     * @param string
+     * @return void
+     */
+    public function setComment($comment)
+    {
+        // section 10-13-1--31-740bb989:119ebfa9b28:-8000:00000000000008E1 begin
+		$this->comment = trim(strip_tags($comment));
+        // section 10-13-1--31-740bb989:119ebfa9b28:-8000:00000000000008E1 end
+    }
+
+    /**
+     * Short description of method remove
+     *
+     * @access public
+     * @author firstname and lastname of author, <author@example.org>
+     * @return void
+     */
+    public function remove()
+    {
+        // section 10-13-1--31-7f1456d9:11a242e5517:-8000:000000000000096F begin
+		$this->resource->delete();
+        // section 10-13-1--31-7f1456d9:11a242e5517:-8000:000000000000096F end
+    }
+
+} /* end of class WfResource */
+
+?>
