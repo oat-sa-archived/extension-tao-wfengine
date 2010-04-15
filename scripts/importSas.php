4208891e409940e948437b621ba42522f6f028ea
bchevrier <bchevrier@793250d4-a296-4af0-ab9f-c080ea916244>
2010-04-15 09:45:26 +0000

    * update service import script
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@726 793250d4-a296-4af0-ab9f-c080ea916244

--- scripts/importSas.php
+++ scripts/importSas.php
@@ -15,6 +15,11 @@ require_once dirname(__FILE__).'/../includes/common.php';
 class SasImporter{
 
 	/**
+	 * @var boolean
+	 */
+	private $outputModeWeb = true;
+	
+	/**
 	 * @var array
 	 */
 	private $services = array();
@@ -23,6 +28,11 @@ class SasImporter{
 	 * @var array
 	 */
 	private $processVars = array();
+	
+	/**
+	 * @var array
+	 */
+	private $formalParams = array();
 	
 	/**
 	 * @var core_kernel_classes_Class
@@ -45,15 +55,49 @@ class SasImporter{
 	private $processVarCodeProp = null;
 	
 	/**
+	 * @var core_kernel_classes_Class
+	 */
+	private $formalParamClass = null;
+	
+	/**
+	 * @var core_kernel_classes_Property
+	 */
+	private $formalParamNameProp = null;
+	
+	/**
+	 * @var core_kernel_classes_Property
+	 */
+	private $formalParamDefProcessVarProp = null;
+	
+	/**
+	 * @var core_kernel_classes_Property
+	 */
+	private $formalParamDefConstantProp = null;
+	
+	/**
 	 * Constructor: init api connection and the ref API resources
 	 */
 	public function __construct(){
-		core_control_FrontController::connect('generis', md5('g3n3r1s'), DATABASE_NAME);
+		
+		if(PHP_SAPI == 'cli'){
+			$this->outputModeWeb = false;
+		}
+		
+		//connec to the api
+		core_control_FrontController::connect(SYS_USER_LOGIN, SYS_USER_PASS, DATABASE_NAME);
 
-		$this->serviceDefClass = new core_kernel_classes_Class(CLASS_SUPPORTSERVICES);
-		$this->serviceUrlProp = new core_kernel_classes_Property(PROPERTY_SERVICESDEFINITION_URL);
-		$this->processVarClass = new core_kernel_classes_Class(CLASS_PROCESSVARIABLES);
-		$this->processVarCodeProp = new core_kernel_classes_Property(PROPERTY_PROCESSVARIABLES_CODE);
+		//initialize ref to API classes and properties
+		
+		$this->serviceDefClass 		= new core_kernel_classes_Class(CLASS_SUPPORTSERVICES);
+		$this->serviceUrlProp 		= new core_kernel_classes_Property(PROPERTY_SERVICESDEFINITION_URL);
+		
+		$this->processVarClass 		= new core_kernel_classes_Class(CLASS_PROCESSVARIABLES);
+		$this->processVarCodeProp 	= new core_kernel_classes_Property(PROPERTY_PROCESSVARIABLES_CODE);
+		
+		$this->formalParamClass 			= new core_kernel_classes_Class(CLASS_FORMALPARAMETER);
+		$this->formalParamNameProp			= new core_kernel_classes_Property(PROPERTY_FORMALPARAMETER_NAME);
+		$this->formalParamDefProcessVarProp	= new core_kernel_classes_Property(PROPERTY_FORMALPARAMETER_DEFAULTPROCESSVARIABLE);
+		$this->formalParamDefConstantProp	= new core_kernel_classes_Property(PROPERTY_FORMALPARAMETER_DEFAULTCONSTANTVALUE);
 	}
 	
 	/**
@@ -61,38 +105,67 @@ class SasImporter{
 	 */
 	public function import(){
 		
-		echo "\n";
+		$this->log();
 		foreach($this->getSasFiles() as $sasFile){
+			log($sasFile." found");
 			$this->parseSasFile($sasFile);
 		}
-		echo "\n";
-		echo "\n";
 		
+		//insert service definitions
 		$serviceNum = count($this->services);
 		$serviceInserted = 0;
 		foreach($this->services as $service){
 			if($this->addService($service['name'], $service['url'], $service['description'])){
 				$serviceInserted++;
-				echo "\r$serviceInserted / $serviceNum  services definition inserted";
+				if(!$this->outputModeWeb){
+					echo "\r$serviceInserted / $serviceNum  services definition inserted";
+				}
 			}
-			
+		}
+		if($this->outputModeWeb){
+			$this->log("$serviceInserted / $serviceNum  services definition inserted");
+		}
+		else{
+			echo "\n";
 		}
 		
-		echo "\n";
-		echo "\n";
-		
+		//insert process vars
 		$processVarNum = count($this->processVars);
 		$processVarInserted = 0;
 		foreach($this->processVars as $processVar){
 			if($this->addProcessVariable($processVar)){
 				$processVarInserted++;
-				echo "\r$processVarInserted / $processVarNum  process variable inserted";
+				if(!$this->outputModeWeb){
+					echo "\r$processVarInserted / $processVarNum  process variable inserted";
+				}
 			}
 		}
+		if($this->outputModeWeb){
+			$this->log("$processVarInserted / $processVarNum  process variable inserted");
+		}
+		else{
+			echo "\n";
+		}
 		
+		//insert formal params
+		$formaParamNum = count($this->formalParams);
+		$formaParamInserted = 0;
+		foreach($this->formalParams as $formalParam){
+			if($this->addFormalParameter($formalParam)){
+				$formaParamInserted++;
+				if(!$this->outputModeWeb){
+					echo "\r$formaParamInserted / $formaParamNum  formal parameters inserted";
+				}
+			}
+		}
+		if($this->outputModeWeb){
+			$this->log("$formaParamInserted / $formaParamNum  formal parameters inserted");
+		}
+		else{
+			echo "\n";
+		}
 		
-		
-		echo "\n";
+		$this->log("import finished");
 	}
 	
 	/**
@@ -133,17 +206,32 @@ class SasImporter{
 				
 				foreach($loc->param as $param){
 					if(isset($param['key'])){
+						
 						$url .= ((string)$param['key']) . '=';
 						if(isset($param['value'])) {
 							$url .= (string)$param['value'];		
+
+							//set the processVars
 							if(!in_array((string)$param['value'], $this->processVars)){
 								$this->processVars[] = (string)$param['value'];
-							}		
+							}	
+
+							//set the formatParams
+							$key = (string)$param['key'].(string)$param['value'];
+							if(!array_key_exists($key, $this->formalParams)){
+								$this->formalParams[$key] = array(
+									'name'			=> (string)$param['key'],
+									'processVar'	=> ( preg_match("/^\^/", (string)$param['value'])) ? (string)$param['value'] : false,
+									'constant'		=> (!preg_match("/^\^/", (string)$param['value'])) ? (string)$param['value'] : false 
+								);
+							}
 						}
 						else{
 							$url .= "^".((string)$param['key']);
 						}
 						$url .= "&";
+						
+						
 					}
 				}
 				if(isset($service->return)){
@@ -174,7 +262,7 @@ class SasImporter{
 	 */
 	private function addService($name, $url,  $description =''){
 		if(!$this->serviceExists($url)){
-			$service = $this->serviceDefClass->createInstance($name, $description);
+			$service = $this->serviceDefClass->createInstance($name, trim($description));
 			if(!is_null($service)){
 				return $service->setPropertyValue($this->serviceUrlProp, $url);
 			}
@@ -234,6 +322,58 @@ class SasImporter{
 		return false;
 	}
 	
+	/**
+	 * get a process var with the code in property
+	 * @param string $code
+	 * @return core_kernel_classes_Resource
+	 */
+	private function getProcessVar($code){
+		
+		foreach($this->processVarClass->getInstances(false) as $processVar){
+			try{
+				if($code == $processVar->getUniquePropertyValue($this->processVarCodeProp)){
+					return $processVar;
+				}
+			}	
+			catch(common_Exception $ce){}		
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param array $formalParam
+	 * @return boolean
+	 */
+	private function addFormalParameter($formalParam){
+		if(is_array($formalParam)){
+			
+			if($formalParam['processVar']){
+				$label = self::unCamelize(str_replace('^', '', $formalParam['processVar'])); 
+			}
+			else{
+				$label = self::unCamelize($formalParam['name']);
+			}
+			
+			
+			$formalParamResource = $this->formalParamClass->createInstance($label);
+			if(!is_null($formalParamResource)){
+				$formalParamResource->setPropertyValue($this->formalParamNameProp, $formalParam['name']);
+				if($formalParam['processVar']){
+					$processVar = $this->getProcessVar(str_replace('^', '', $formalParam['processVar']));
+					if(!is_null($processVar)){
+						$formalParamResource->setPropertyValue($this->formalParamDefProcessVarProp, $processVar->uriResource);
+					}
+				}
+				if($formalParam['constant']){
+					$formalParamResource->setPropertyValue($this->formalParamDefConstantProp, $formalParam['constant']);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	
 	/**
 	 * Utility method to (unCamelize -> un camelize) a string
@@ -249,7 +389,26 @@ class SasImporter{
 		}
 		return implode(' ', $output);
 	}
+	
+	
+	/**
+	 * @param string $message
+	 */
+	private function log($message = ''){
+		if($this->outputModeWeb){
+			echo "{$message}</br>";
+		}
+		else{
+			echo "{$message}\n";
+		}
+	}
+	
 }
+
+/*
+ * Run the importer
+ */
 $importer = new SasImporter();
 $importer->import();
+
 ?>
