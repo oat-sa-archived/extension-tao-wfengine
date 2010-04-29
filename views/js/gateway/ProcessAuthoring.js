b85c66cc6ac871c00adf0fcbc0bb7568ad66d2a4
ssipasseuth <ssipasseuth@793250d4-a296-4af0-ab9f-c080ea916244>
2010-04-29 13:50:51 +0000

    ProcessAuthoring.js 'gateway' added
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@748 793250d4-a296-4af0-ab9f-c080ea916244

---
+++ views/js/gateway/ProcessAuthoring.js
@@ -0,0 +1,46 @@
+// alert("gtw loaded");
+var GatewayProcessAuthoring = {name: 'process authoring ontology gateway'};
+
+GatewayProcessAuthoring.addActivity = function(url, processUri){
+	// console.log('url', url);	
+	// console.log('processUri', processUri);
+	
+	$.ajax({
+		url: url,
+		type: "POST",
+		data: {processUri: processUri, type: 'activity'},
+		dataType: 'json',
+		success: function(response){
+			// console.log(response);
+			if (response.uri) {
+				EventMgr.trigger('activityAdded', response);
+			}else{
+				console.log('error in adding an activity');
+			}
+		}
+	});
+	
+}
+
+GatewayProcessAuthoring.addInteractiveService = function(url, activityUri, serviceDefinitionUri){
+	// console.log('url', url);	
+	// console.log('processUri', processUri);
+	data = {activityUri: options.id, type: 'interactive-service'};
+	if(serviceDefinitionUri){
+		data.serviceDefinitionUri = serviceDefinitionUri;
+	}
+	
+	$.ajax({
+		url: url,
+		type: "POST",
+		data: data,
+		dataType: 'json',
+		success: function(response){
+			if (response.uri) {
+				EventMgr.trigger('interactiveServiceAdded', response);
+				
+			}
+		}
+	});
+	
+}
