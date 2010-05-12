212c92b8de77f7e3db5a27e69a12633b9688ffa3
bchevrier <bchevrier@793250d4-a296-4af0-ab9f-c080ea916244>
2010-05-12 10:03:36 +0000

    * implement  Property::setDomain to enable multi domains  * create a TestSuite for tao to run all the TAO* extensions unit tests  * add the class cloning into the abstract service  * refactor the getProperties between 2 classes into the abtract service  * add the taoQual extension  * add the remove method in the wfEngine/actions/ServiceAPi to remove/reset the process shared variables
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@765 793250d4-a296-4af0-ab9f-c080ea916244

--- actions/ServicesApi.class.php
+++ actions/ServicesApi.class.php
@@ -39,4 +39,31 @@ class ServiceApi extends Module
 		return false;
 	}
 	
+	/**
+	 * Service persitance remover 
+	 * @param mixed $params the list of param keys you want to remove
+	 * @return boolean
+	 */
+	public static function remove($params){
+		
+		if(isset($_SESSION["processUri"])){
+			$processUri = $_SESSION["processUri"];
+			$process = new core_kernel_classes_Resource(urldecode($processUri));
+			if(is_string($params)){
+				$params = array($params);
+			}
+			if(is_array($params)){
+				foreach($params as $param) {
+					$collection = core_kernel_impl_ApiModelOO::singleton()->getSubject(PROPERTY_CODE, $param);
+					if(!$collection->isEmpty()){
+						if($collection->count() == 1) {
+							$property = new core_kernel_classes_Property($collection->get(0)->uriResource);
+							return $process->removePropertyValue($property, $param);
+						}
+					}
+				}
+			}
+		}
+	}
+	
 }
