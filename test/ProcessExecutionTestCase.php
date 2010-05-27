2b7e6b4f48f422085cb80d2572306cd147871495
llecaque <llecaque@793250d4-a296-4af0-ab9f-c080ea916244>
2010-05-27 11:13:18 +0000

    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@794 793250d4-a296-4af0-ab9f-c080ea916244

--- test/ProcessExecutionTestCase.php
+++ test/ProcessExecutionTestCase.php
@@ -23,7 +23,7 @@ error_reporting(E_ALL);
 
 class ProcessExecutionTestCase extends UnitTestCase{
 	
-	protected $procDefinition = null;
+
 	protected $apiModel = null;
 	
 	public function setUp(){
@@ -33,7 +33,7 @@ class ProcessExecutionTestCase extends UnitTestCase{
 
 	}
 	
-	public function testCreateProcess(){
+	public function testCreateSplitProcess(){
 	
 		$authoringService = tao_models_classes_ServiceFactory::get('wfEngine_models_classes_ProcessAuthoringService');
 		
@@ -108,7 +108,73 @@ class ProcessExecutionTestCase extends UnitTestCase{
 		
 	}
 	
+	public function testCreateJoinProcess(){
+		
+		$authoringService = tao_models_classes_ServiceFactory::get('wfEngine_models_classes_ProcessAuthoringService');
+		$processDefinitionClass = new core_kernel_classes_Class(CLASS_PROCESS);
+		$processDefinition = $processDefinitionClass->createInstance('processForUnitTest_' . date(DATE_ISO8601),'created for the unit test of process execution');
+		
+		$activity0 = $authoringService->createActivity($processDefinition, 'activity0');
+		$authoringService->setFirstActivity($processDefinition,$activity0);
+		$connector0 = $authoringService->createConnector($activity0);
+		
+		$connectorParallele = new core_kernel_classes_Resource(INSTANCE_TYPEOFCONNECTORS_PARALLEL);
+		$authoringService->setConnectorType($connector0,$connectorParallele);
+		
+		$parallelActivity1 = $authoringService->createActivity($processDefinition, 'myActivity1');
+		$connector1 = $authoringService->createConnector($parallelActivity1);
 
+		$parallelActivity2 = $authoringService->createActivity($processDefinition, 'myActivity2');
+		$connector2 = $authoringService->createConnector($parallelActivity2);
+		
+		$joinActivity = $authoringService->createActivity($processDefinition, 'joinActivity');
+		
+		//join parallel Activity 1 and 2 to "joinActivity"
+		$this->assertIsA($authoringService->createJoinActivity($connector1, $joinActivity), 'core_kernel_classes_Resource');
+		$authoringService->createJoinActivity($connector2, $joinActivity);
+		
+		//both connectors joined to the same activity have the same transition rule?
+		$propTransitionRule = new core_kernel_classes_Property(PROPERTY_CONNECTORS_TRANSITIONRULE);
+		$transitionRule1 = $connector1->getUniquePropertyValue($propTransitionRule);
+		$this->assertIsA($transitionRule1, 'core_kernel_classes_Resource');
+		$transitionRule2 = $connector2->getUniquePropertyValue($propTransitionRule);
+		$this->assertEqual($transitionRule1->uriResource, $transitionRule2->uriResource);
+
+		//same activity in 'then' property?
+		$propThen = new core_kernel_classes_Property(PROPERTY_TRANSITIONRULES_THEN);
+		$this->assertEqual($transitionRule1->getUniquePropertyValue($propThen)->uriResource, $joinActivity->uriResource);
+		
+		//test update of the joined activity after a connector has been disonnected from it:
+		$oldConditonIf = $transitionRule1->getUniquePropertyValue(new core_kernel_classes_Property(PROPERTY_RULE_IF));
+		$connector2->removePropertyValues(new core_kernel_classes_Property(PROPERTY_CONNECTORS_NEXTACTIVITIES));
+		$authoringService->updateJoinedActivity($joinActivity);
+		
+		//the condition of transition rule of the connector 1 has been modified?
+		$newConditionIf = $transitionRule1->getUniquePropertyValue(new core_kernel_classes_Property(PROPERTY_RULE_IF));
+		$this->assertNotEqual($oldConditonIf->uriResource, $newConditionIf->uriResource);
+		// var_dump($transitionRule1, $transitionRule1bis);
+		
+		
+		$factory = new ProcessExecutionFactory();
+		$factory->name = 'Test Process Execution Parallele';
+		$factory->execution = $processDefinition->uriResource;
+		$factory->ownerUri = LOGIN;
+
+		$proc = $factory->create();
+		$proc->performTransition();
+		var_dump($proc->currentActivity);
+		
+		$parallelActivity1->delete();
+		$connector1->delete();
+		$parallelActivity2->delete();
+		$connector2->delete();
+		$transitionRule1->delete();//TODO test all delete methods:
+		
+		$proc->resource->delete();
+			
+		//delete processdef:
+//		$authoringService->deleteProcess($processDefinition);
+	}
 	
 }
 
