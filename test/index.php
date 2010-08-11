1312840d9753a8c6826edbd771b793904cd89053
bchevrier <bchevrier@793250d4-a296-4af0-ab9f-c080ea916244>
2010-08-11 13:42:40 +0000

    * update unti testing mode: one suite by module
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@916 793250d4-a296-4af0-ab9f-c080ea916244

---
+++ test/index.php
@@ -0,0 +1,24 @@
+<?php
+set_time_limit(0);
+
+require_once dirname(__FILE__) . '/../../tao/test/TestRunner.php';
+
+//get the test into each extensions
+$tests = TestRunner::getTests(array('wfEngine'));
+
+//create the test sutie
+$testSuite = new TestSuite('workflow engine unit tests');
+foreach($tests as $testCase){
+	$testSuite->addTestFile($testCase);
+}    
+
+//add the reporter regarding the context
+if(PHP_SAPI == 'cli'){
+	$reporter = new XmlTimeReporter();
+}
+else{
+	$reporter = new HtmlReporter();
+}
+//run the unit test suite
+$testSuite->run($reporter);
+?>
