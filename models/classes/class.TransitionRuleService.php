7f19314c39c908847aeddbbb318dfacb7e71c021
llecaque <llecaque@793250d4-a296-4af0-ab9f-c080ea916244>
2011-09-16 12:37:21 +0000

    add getExpression in TransitionRuleService
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@1231 793250d4-a296-4af0-ab9f-c080ea916244

--- models/classes/class.TransitionRuleService.php
+++ models/classes/class.TransitionRuleService.php
@@ -9,10 +9,10 @@ error_reporting(E_ALL);
  *
  * This file is part of TAO.
  *
- * Automatically generated on 05.09.2011, 19:16:11 with ArgoUML PHP module 
+ * Automatically generated on 16.09.2011, 14:35:59 with ArgoUML PHP module 
  * (last revised $Date: 2010-01-12 20:14:42 +0100 (Tue, 12 Jan 2010) $)
  *
- * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
+ * @author Lionel Lecaque, <lionel.lecaque@tudor.lu>
  * @package wfEngine
  * @subpackage models_classes
  */
@@ -24,7 +24,7 @@ if (0 > version_compare(PHP_VERSION, '5')) {
 /**
  * include wfEngine_models_classes_RuleService
  *
- * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
+ * @author Lionel Lecaque, <lionel.lecaque@tudor.lu>
  */
 require_once('wfEngine/models/classes/class.RuleService.php');
 
@@ -40,7 +40,7 @@ require_once('wfEngine/models/classes/class.RuleService.php');
  * Short description of class wfEngine_models_classes_TransitionRuleService
  *
  * @access public
- * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
+ * @author Lionel Lecaque, <lionel.lecaque@tudor.lu>
  * @package wfEngine
  * @subpackage models_classes
  */
@@ -58,7 +58,7 @@ class wfEngine_models_classes_TransitionRuleService
      * Short description of method getElseActivity
      *
      * @access public
-     * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
+     * @author Lionel Lecaque, <lionel.lecaque@tudor.lu>
      * @param  Resource rule
      * @return core_kernel_classes_Resource
      */
@@ -76,7 +76,7 @@ class wfEngine_models_classes_TransitionRuleService
      * Short description of method getThenActivity
      *
      * @access public
-     * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
+     * @author Lionel Lecaque, <lionel.lecaque@tudor.lu>
      * @param  Resource rule
      * @return core_kernel_classes_Resource
      */
@@ -94,7 +94,7 @@ class wfEngine_models_classes_TransitionRuleService
      * Short description of method isTransitionRule
      *
      * @access public
-     * @author Somsack Sipasseuth, <somsack.sipasseuth@tudor.lu>
+     * @author Lionel Lecaque, <lionel.lecaque@tudor.lu>
      * @param  Resource rule
      * @return boolean
      */
@@ -106,6 +106,24 @@ class wfEngine_models_classes_TransitionRuleService
         // section 127-0-1-1-66b8afb4:1322473370c:-8000:0000000000002EDE end
 
         return (bool) $returnValue;
+    }
+
+    /**
+     * Short description of method getExpression
+     *
+     * @access public
+     * @author Lionel Lecaque, <lionel.lecaque@tudor.lu>
+     * @param  Resource rule
+     * @return core_kernel_classes_Expression
+     */
+    public function getExpression( core_kernel_classes_Resource $rule)
+    {
+        $returnValue = null;
+
+        // section 127-0-1-1-74734511:1327233d503:-8000:0000000000003032 begin
+        // section 127-0-1-1-74734511:1327233d503:-8000:0000000000003032 end
+
+        return $returnValue;
     }
 
 } /* end of class wfEngine_models_classes_TransitionRuleService */
