2db39eaf43a129a364fbf0e9ddd08b38e768aef9
llecaque <llecaque@793250d4-a296-4af0-ab9f-c080ea916244>
2011-11-16 11:14:54 +0000

    wfApi
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@1325 793250d4-a296-4af0-ab9f-c080ea916244

---
+++ actions/class.WfApi.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Service interface to save and retrieve recovery contexts
+ * 
+ * @author CRP Henri Tudor - TAO Team - {@link http://www.tao.lu}
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * @package wfEngine
+ * @subpackage actions
+ *
+ */
+class wfEngine_actions_WfApi extends tao_actions_Api {
+    
+    
+}
