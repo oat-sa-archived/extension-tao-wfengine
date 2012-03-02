a7232137400f7b48d8f66283de8e5963d3b19996
jbogaerts <jbogaerts@793250d4-a296-4af0-ab9f-c080ea916244>
2012-03-02 10:39:32 +0000

    Fix issue #1126.
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@1400 793250d4-a296-4af0-ab9f-c080ea916244

--- actions/class.WfModule.php
+++ actions/class.WfModule.php
@@ -10,18 +10,7 @@ class wfEngine_actions_WfModule extends Module
 			$userService->connectCurrentUser();
 		}
 		else{
-			if($this->hasRequestParameter('processUri') && $this->hasRequestParameter('activityUri')){
-				$this->redirect(_url('index', 'Authentication', 'wfEngine', array(
-						'errorMessage' => urlencode(__('Please login to access the selected activity.')),
-						'processUri' => urlencode($this->getRequestParameter('processUri')),
-						'activityUri' => urlencode($this->getRequestParameter('activityUri'))
-						)
-					));
-			}else{
-				$this->redirect(_url('index', 'Authentication', 'wfEngine', array(
-						'errorMessage' => urlencode(__('Access denied. Please renew your authentication.'))
-					)));
-			}
+			$this->notAllowedRedirection();
 		}
 		
 	}
@@ -35,5 +24,24 @@ class wfEngine_actions_WfModule extends Module
 	{
 		return (isset($_SESSION['taoqual.authenticated']) && core_kernel_users_Service::singleton()->isASessionOpened());	//if a user is logged in
 	}
+    
+    /**
+     * Behaviour to adopt if the user is not allowed to access the current action.
+     */
+    protected function notAllowedRedirection()
+    {
+        if($this->hasRequestParameter('processUri') && $this->hasRequestParameter('activityUri')){
+            $this->redirect(_url('index', 'Authentication', 'wfEngine', array(
+                    'errorMessage' => urlencode(__('Please login to access the selected activity.')),
+                    'processUri' => urlencode($this->getRequestParameter('processUri')),
+                    'activityUri' => urlencode($this->getRequestParameter('activityUri'))
+                    )
+                ));
+        }else{
+            $this->redirect(_url('index', 'Authentication', 'wfEngine', array(
+                    'errorMessage' => urlencode(__('Access denied. Please renew your authentication.'))
+                )));
+        }
+    }
 }
 ?>
