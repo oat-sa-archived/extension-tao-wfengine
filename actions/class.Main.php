c007343f85bae7676be179397d93b6dd732857d0
jbout <jbout@793250d4-a296-4af0-ab9f-c080ea916244>
2012-09-19 14:08:06 +0000

    * redirected getTree/getActions to extension for translation * fixed manifests
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@1516 793250d4-a296-4af0-ab9f-c080ea916244

---
+++ actions/class.Main.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Subjects Controller provide actions performed from url resolution
+ * 
+ * @author Bertrand Chevrier, <taosupport@tudor.lu>
+ * @package wfEngine
+ * @subpackage actions
+ * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
+ * 
+ */
+
+class wfEngine_actions_Main extends tao_actions_Main {
+	
+}
+?>
