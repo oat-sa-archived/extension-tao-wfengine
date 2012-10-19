635019bdeeb40da323ed2bb7a933a239e841af46
llecaque <llecaque@793250d4-a296-4af0-ab9f-c080ea916244>
2012-10-19 09:30:11 +0000

    bad copy paste in code coverage
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@1545 793250d4-a296-4af0-ab9f-c080ea916244

--- test/coverage/generateCoverage.php
+++ test/coverage/generateCoverage.php
@@ -2,10 +2,10 @@
 require_once dirname(__FILE__) . '/../../../tao/test/TestRunner.php';
 
 //get the test into each extensions
-$tests = TestRunner::getTests(array('taoSubjects'));
+$tests = TestRunner::getTests(array('wfEngine'));
 
 //create the test sutie
-$testSuite = new TestSuite('TAO Subject unit tests');
+$testSuite = new TestSuite('TAO wfEngine unit tests');
 foreach($tests as $testCase){
 	$testSuite->addFile($testCase);
 }    
