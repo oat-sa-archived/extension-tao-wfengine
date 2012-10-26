794be17daed03120b81cf1ba8e13c39caffe873e
jbihin <jbihin@793250d4-a296-4af0-ab9f-c080ea916244>
2012-10-26 13:19:40 +0000

    fix Monitor::monitorProcess
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@1552 793250d4-a296-4af0-ab9f-c080ea916244

--- actions/class.Monitor.php
+++ actions/class.Monitor.php
@@ -1,16 +1,16 @@
 <?php
 /**
  *  Montitor Controler provide actions to manage processes
- * 
+ *
  * @author Cédric Alfonsi, <taosupport@tudor.lu>
  * @package wfEngine
  * @subpackage actions
  * @license GPLv2  http://www.opensource.org/licenses/gpl-2.0.php
  */
 class wfEngine_actions_Monitor extends tao_actions_TaoModule {
-	
+
 	protected $variableService = null;
-	
+
 	/**
 	 * Constructor
 	 */
@@ -35,18 +35,18 @@ class wfEngine_actions_Monitor extends tao_actions_TaoModule {
 				)
 			)
 		);
-		
+
 		$this->variableService = wfEngine_models_classes_VariableService::singleton();
 	}
-	
+
 	/**
-	 * 
+	 *
 	 */
 	public function getRootClass()
 	{
 		return null;
 	}
-	
+
 	/**
 	 * The monitoring front page
 	 * -> Display current process status
@@ -57,17 +57,17 @@ class wfEngine_actions_Monitor extends tao_actions_TaoModule {
 	{
 		//Class to filter on
 		$clazz = new core_kernel_classes_Class(CLASS_PROCESSINSTANCES);
-		
+
 		//Properties to filter on
 		$properties = array();
 		$properties[] = new core_kernel_classes_Property(PROPERTY_PROCESSINSTANCES_EXECUTIONOF);
 		$properties[] = new core_kernel_classes_Property(PROPERTY_PROCESSINSTANCES_STATUS);
-		
+
 		//Monitoring grid
 		$processMonitoringGrid = new wfEngine_helpers_Monitoring_ProcessMonitoringGrid(array(), $this->processMonitoringGridOptions);
 		$grid = $processMonitoringGrid->getGrid();
 		$model = $grid->getColumnsModel();
-		
+
 		//Process history grid
 		$processHistoryGrid = new wfEngine_helpers_Monitoring_ExecutionHistoryGrid(new core_kernel_classes_Resource(' '), array(
 			'columns' => array(
@@ -80,64 +80,63 @@ class wfEngine_actions_Monitor extends tao_actions_TaoModule {
 			)
 		));
 		$historyProcessModel = $processHistoryGrid->getGrid()->getColumnsModel();
-		
+
 		//Filtering data
 		$this->setData('clazz', $clazz);
 		$this->setData('properties', $properties);
-		
+
 		//Monitoring data
 		$this->setData('model', json_encode($model));
 		$this->setData('historyProcessModel', json_encode($historyProcessModel));
 		$this->setData('data', $processMonitoringGrid->toArray());
-		
+
 		//WF Variables
 		$this->setData('wfVariables', json_encode($this->variableService->getAllVariables()));
-		
+
 		$this->setView('monitor/index.tpl');
 	}
-	
+
 	/**
 	 * Get JSON monitoring data
 	 */
 	public function monitorProcess()
 	{
 		$returnValue = array();
-		$filter = null;
-		
+		$filters = null;
+
 		//get the filter
-		if($this->hasRequestParameter('filter')){
+		if ($this->hasRequestParameter('filter')) {
 			$filter = $this->getRequestParameter('filter');
 			$filter = $filter == 'null' || empty($filter) ? null : $filter;
-            if(is_array($filter)){
-                foreach($filter as $propertyUri=>$propertyValues){
-                    foreach($propertyValues as $i=>$propertyValue){
-                        $propertyDecoded = tao_helpers_Uri::decode($propertyValue);
-                        if(common_Utils::isUri($propertyDecoded)){
-                            $filter[$propertyUri][$i] = $propertyDecoded;
-                        }
-                    }
-                }
-            }
-		}
-		//get the processes uris
-		$processesUri = $this->hasRequestParameter('processesUri') ? $this->getRequestParameter('processesUri') : null;
-		
-		$processInstancesClass = new core_kernel_classes_Class(CLASS_PROCESSINSTANCES);
-		if(!is_null($filter)){
-			$processExecutions = $processInstancesClass->searchInstances($filter, array ('recursive'=>true));
-		}
-		else if(!is_null($processesUri)){
-			foreach($processesUri as $processUri){
-				$processExecutions[$processUri] = new core_kernel_classes_resource($processUri);
+			if (is_array($filter)) {
+				foreach ($filter as $propertyUri => $propertyValues) {
+					foreach ($propertyValues as $i => $propertyValue) {
+						$propertyDecoded = tao_helpers_Uri::decode($propertyValue);
+						if (common_Utils::isUri($propertyDecoded)) {
+								$filters[tao_helpers_Uri::decode($propertyUri)][$i] = $propertyDecoded;
+						}
+					}
+				}
 			}
 		}
-		else{
+
+		//get the processes uris
+		$processesUri = $this->hasRequestParameter('processesUri') ? $this->getRequestParameter('processesUri') : null;
+
+		$processInstancesClass = new core_kernel_classes_Class(CLASS_PROCESSINSTANCES);
+		if (!is_null($filters)) {
+			$processExecutions = $processInstancesClass->searchInstances($filters, array ('recursive'=>true));
+		} else if(!is_null($processesUri)) {
+			foreach ($processesUri as $processUri) {
+				$processExecutions[$processUri] = new core_kernel_classes_resource($processUri);
+			}
+		} else {
 			$processExecutions = $processInstancesClass->getInstances();
 		}
-		
+
 		$processMonitoringGrid = new wfEngine_helpers_Monitoring_ProcessMonitoringGrid(array_keys($processExecutions), $this->processMonitoringGridOptions);
 		$data = $processMonitoringGrid->toArray();
-		
+
 		echo json_encode($data);
 	}
 
@@ -149,10 +148,10 @@ class wfEngine_actions_Monitor extends tao_actions_TaoModule {
 		if($this->hasRequestParameter('uri')){
 			$uri = $this->getRequestParameter('uri');
 		}
-		
+
 		$processMonitoringGrid = new wfEngine_helpers_Monitoring_ExecutionHistoryGrid(new core_kernel_classes_Resource($uri));
 		$data = $processMonitoringGrid->toArray();
-		
+
 		echo json_encode($data);
 	}
 }
