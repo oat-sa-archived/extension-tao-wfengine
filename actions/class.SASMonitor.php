65c56c09cf854d5ce8c154a13ce0462baee8e6e9
jbout <jbout@793250d4-a296-4af0-ab9f-c080ea916244>
2013-06-12 15:34:07 +0000

    SAS setView() cleanup
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@1657 793250d4-a296-4af0-ab9f-c080ea916244

--- actions/class.SASMonitor.php
+++ actions/class.SASMonitor.php
@@ -41,24 +41,5 @@ class wfEngine_actions_SASMonitor extends wfEngine_actions_Monitor {
     	tao_helpers_Context::load('STANDALONE_MODE');
         parent::__construct();
 	}
-    
-    /**
-	 * @see TaoModule::setView()
-	 * @param string $identifier the view name
-	 * @param boolean $useMetaExtensionView use a view from the parent extention
-	 * @return mixed 
-	 */
-    public function setView($identifier, $useMetaExtensionView = false) {
-		if(tao_helpers_Request::isAjax()){
-			return parent::setView($identifier, $useMetaExtensionView);
-		}
-    	if($useMetaExtensionView){
-			$this->setData('includedView', $identifier);
-		}
-		else{
-			$this->setData('includedView', DIR_VIEWS . 'templates/' . $identifier);
-		}
-		return parent::setView('sas.tpl', true);
-    }
 	
 }
