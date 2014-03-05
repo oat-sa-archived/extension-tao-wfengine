564381a08f9c6eda3939f6d25d191ae8b9563a99
jbout <jbout@793250d4-a296-4af0-ab9f-c080ea916244>
2014-03-05 17:12:08 +0000

    cleanup extension config rework
    git-svn-id: http://vcs.taotesting.com/svn/taoqual/branches/WorlflowEngineExt@1755 793250d4-a296-4af0-ab9f-c080ea916244

--- manifest.php
+++ manifest.php
@@ -48,10 +48,7 @@ return array(
 		),
 		'php' => array(
 			//dirname(__FILE__). '/scripts/importSas.php',
-		),
-		'checks' => array(
-			array('type' => 'CheckFileSystemComponent', 'value' => array('id' => 'fs_wfEngine_includes', 'location' => 'wfEngine/includes', 'rights' => 'rw'))
-		),
+		)
 	),
 	'managementRole' => 'http://www.tao.lu/middleware/wfEngine.rdf#WorkflowsManagerRole',
     'acl' => array(
