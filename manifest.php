fe4911f21c11e5ac153cd39200e535e1e99f9377
Bertrand Chevrier <bertrand@taotesting.com>
2014-11-06 16:22:30 +0100

    bump tao dep to 2.7.0

--- manifest.php
+++ manifest.php
@@ -36,7 +36,7 @@ return array(
 	'version' => '2.6',
 	'author' => 'Open Assessment Technologies, CRP Henri Tudor',
 	'requires' => array(
-	    'tao' => '>=2.4'
+	    'tao' => '>=2.7.0'
     ),
 	'models' => array(
 		'http://www.tao.lu/middleware/wfEngine.rdf'
