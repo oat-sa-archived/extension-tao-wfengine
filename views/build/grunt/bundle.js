f0ac0e0b0b3e76a78f7044fd179d37fb5443e3c8
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-02-03 11:31:56 +0100

    fix parrallel build

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
     /**
      * Remove bundled and bundling files
      */
-    clean.wfenginebundle = [out,  root + '/wfEngine/views/js/controllers.min.js'];
+    clean.wfenginebundle = [out];
     
     /**
      * Compile tao files into a bundle 
